0127b566ae767ccdc09726e119f9fb9d88433d07
oscaraha <oscarah01@gmail.com>
2021-12-16 14:23:58 +0100

    added images

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,8 +1,12 @@
+import logo from './images/Air.png';
 import React from 'react'
 
 const Game = () => {
+    
+
     return (
         <div>
+            <img src={logo} alt="Air image"></img>
         </div>
     )
 }
