ef5e5e85942fcfdf77dc5f3becfecc4e228b0954
oscaraha <oscarah01@gmail.com>
2021-12-16 08:21:20 +0100

    added core and some ui

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <div>
+            <h1 className="App-header">Minesweeper</h1>
+        </div>
+    )
+}
+
+export default Header
