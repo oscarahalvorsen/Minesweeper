229c541ce5bd25ee59060dbd0bf90150cf1e2c9a
oscaraha <oscarah01@gmail.com>
2021-12-26 21:05:48 +0100

    tutorial video 3 at 23:13

---
+++ djangofolder/frontend/src/components/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import { render } from "react-dom";
+
+export default class App extends Component {
+    constructor(props) {
+        super(props)
+    }
+    render() {
+        return (<h1>Testing djangofolder.frontend.src.components.App.js</h1>)
+    }
+}
